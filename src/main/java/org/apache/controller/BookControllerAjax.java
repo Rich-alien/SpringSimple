970d16a6e2b2a054ece1249d8deaf33d2aa55616
rich-alien <Richalien@yandex.ru>
2020-12-21 01:49:03 +0400

    controller ajax

---
+++ src/main/java/org/apache/controller/BookControllerAjax.java
@@ -0,0 +1,43 @@
+package org.apache.controller;
+
+import org.apache.model.Book;
+import org.apache.service.BookHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class BookControllerAjax {
+    @Autowired
+    private BookHolder bookHolder;
+    @RequestMapping(value = "books/ajax", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Book> bookListAjax(){
+        return bookHolder.getBooks();
+    }
+    @RequestMapping(value = "books/ajax/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Book getBookAjax(@PathVariable("id") int id){
+        return bookHolder.getBookById(id);
+    }
+    @RequestMapping(value = "/books/add/ajax", method = RequestMethod.POST)
+    public @ResponseBody Book addBookAjax(@RequestBody Book book){
+        bookHolder.addBook(book);
+        return book;
+    }
+    @RequestMapping(value = "books/remove/ajax/{id}", method =
+            RequestMethod.DELETE)
+    @ResponseBody
+    public Book removeBookAjax(@PathVariable("id") int id) {
+        Book book = bookHolder.getBookById(id);
+        bookHolder.removeBook(id);
+        return book;
+    }
+    @RequestMapping(value = "books/edit/ajax")
+    @ResponseBody
+    public Book editBook(@RequestBody Book book){
+        Book bookObj = bookHolder.getBookById(book.getId());
+        bookHolder.updateBook(book);
+        return bookObj;
+    }
+}
