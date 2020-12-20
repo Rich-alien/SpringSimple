886516ad34c77c595fef82a16987ee97dc943d75
rich-alien <Richalien@yandex.ru>
2020-12-20 21:36:09 +0400

    создан класс для открытия index.html  при запросе /

--- src/main/java/org/apache/controller/BookController.java
+++ src/main/java/org/apache/controller/BookController.java
@@ -1,4 +1,10 @@
 package org.apache.controller;
-import
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class BookController {
+    @RequestMapping(value = "/")
+    public String index(){
+        return "index";
+    }
 }
