4a8f2fccede3631799962a72714919cd563a0351
rich-alien <Richalien@yandex.ru>
2020-12-21 02:35:56 +0400

    fix

---
+++ src/main/java/org/apache/ApplicationStart.java
@@ -0,0 +1,18 @@
+package org.apache;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import javax.swing.*;
+
+@Configuration
+@ComponentScan
+public class ApplicationStart {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationStart.class);
+
+    }
+}
